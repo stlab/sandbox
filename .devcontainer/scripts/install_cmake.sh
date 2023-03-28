3f29d1e59c8edbd745d252c38660b7bb47b12539
nickpdemarco <nickpdemarco@gmail.com>
2023-03-28 16:48:01 -0400

    Move cmake install to a script with platform detection

---
+++ .devcontainer/scripts/install_cmake.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+CMAKE_VERSION=3.25.2
+
+apt-get update && \
+apt-get -y install build-essential && \
+apt-get install -y wget && \
+rm -rf /var/lib/apt/lists/* && \
+wget https://github.com/Kitware/CMake/releases/download/v${CMAKE_VERSION}/cmake-${CMAKE_VERSION}-linux-$(uname -m).sh -q -O /tmp/cmake-install.sh && \
+chmod u+x /tmp/cmake-install.sh && \
+mkdir /opt/cmake-${CMAKE_VERSION} && \
+/tmp/cmake-install.sh --skip-license --prefix=/opt/cmake-${CMAKE_VERSION} && \
+rm /tmp/cmake-install.sh && \
+ln -s /opt/cmake-${CMAKE_VERSION}/bin/* /usr/local/bin
